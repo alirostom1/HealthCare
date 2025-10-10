b3846126473bd54a59feffc4fae5e81976ccf12f
ali-rostom1 <ali.rostom220@gmail.com>
2025-10-10 22:34:04 +0100

    feat: implement PatientService and PatientServiceImpl with patient registration, queue management, and vital signs handling

---
+++ src/main/java/io/github/alirostom1/heartline/service/PatientService.java
@@ -0,0 +1,23 @@
+package io.github.alirostom1.heartline.service;
+
+import io.github.alirostom1.heartline.model.entity.Patient;
+import io.github.alirostom1.heartline.model.entity.VitalSigns;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PatientService {
+    Patient registerPatient(Patient patient, VitalSigns vitalSigns);
+    Patient updatePatient(Patient patient);
+    Optional<Patient> findPatientBySsn(String ssn);
+    List<Patient> findPatientsByFullName(String fullName);
+    List<Patient> getTodayPatients();
+    List<Patient> getAllPatients();
+
+    List<Patient> getQueuePatients();
+    void addToQueue(UUID patientId);
+    void removeFromQueue(UUID patientId);
+
+    Patient addVitalSigns(UUID patientId, VitalSigns vitalSigns);
+}
