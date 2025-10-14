0cdecde12927344f3456371c00ed3041074a7daf
ali-rostom1 <ali.rostom220@gmail.com>
2025-10-14 10:25:41 +0100

    feat: implement methods to add and remove medical acts in ConsultationRepoImpl

--- src/main/java/io/github/alirostom1/heartline/repository/ConsultationRepoImpl.java
+++ src/main/java/io/github/alirostom1/heartline/repository/ConsultationRepoImpl.java
@@ -1,6 +1,7 @@
 package io.github.alirostom1.heartline.repository;
 
 import io.github.alirostom1.heartline.model.entity.Consultation;
+import io.github.alirostom1.heartline.model.entity.MedicalAct;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -33,7 +34,6 @@ public class ConsultationRepoImpl implements ConsultationRepo{
             return savedConsult;
         }catch(Exception e){
             if(tx.isActive()) tx.rollback();
-            e.printStackTrace();
             throw new RuntimeException("Failed to save consultation");
         }finally {
             em.close();
@@ -47,11 +47,83 @@ public class ConsultationRepoImpl implements ConsultationRepo{
 
     @Override
     public Optional<Consultation> findById(UUID id) {
-        return Optional.empty();
+        EntityManager em = emf.createEntityManager();
+        try{
+            return Optional.ofNullable(em.find(Consultation.class,id));
+        }finally {
+            em.close();
+        }
     }
 
     @Override
     public List<Consultation> findAll() {
-        return List.of();
+        EntityManager em = emf.createEntityManager();
+        try{
+            return em.createQuery("SELECT c FROM Consultation c ORDER BY c.createdAt DESC",Consultation.class).getResultList();
+        }finally {
+            em.close();
+        }
+    }
+    @Override
+    public List<MedicalAct> getAllMedicalActs(){
+        EntityManager em = emf.createEntityManager();
+        try{
+            return em.createQuery("SELECT ma FROM MedicalAct ma ORDER BY ma.price ASC",MedicalAct.class).getResultList();
+        }finally {
+            em.close();
+        }
+    }
+    @Override
+    public Consultation addMedicalAct(UUID consultationId,UUID medicalActId){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try{
+            tx.begin();
+            Consultation consultation = em.find(Consultation.class,consultationId);
+            if(consultation == null){
+                throw new IllegalArgumentException("Consultation not found with id: " + consultationId);
+            }
+            MedicalAct medicalAct = em.find(MedicalAct.class,medicalActId);
+            if(medicalAct == null){
+                throw new IllegalArgumentException("Medical act not found with id : " + medicalActId);
+            }
+            consultation.getMedicalActs().add(medicalAct);
+            em.merge(consultation);
+            tx.commit();
+            return consultation;
+        }catch(Exception e){
+            if(tx.isActive()) tx.rollback();
+            throw new RuntimeException("Failed to add medical Act");
+        }finally {
+            em.close();
+        }
+    }
+    @Override
+    public Consultation removeMedicalAct(UUID consultationId,UUID medicalActId){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Consultation consultation = em.find(Consultation.class,consultationId);
+            if(consultation == null){
+                throw new IllegalArgumentException("Consultation not found with id : " + consultationId);
+            }
+            MedicalAct medicalAct = em.find(MedicalAct.class,medicalActId);
+            if(medicalAct == null){
+                throw new IllegalArgumentException("Medical act not found with id: " + medicalActId);
+            }
+            if(!consultation.getMedicalActs().contains(medicalAct)){
+                throw new IllegalArgumentException("Medical act isn't attached to consultation!");
+            }
+            consultation.getMedicalActs().remove(medicalAct);
+            em.merge(medicalAct);
+            tx.commit();
+            return consultation;
+        }catch (Exception e){
+            if(tx.isActive()) tx.rollback();
+            throw new RuntimeException("Failed to remove medical act : " + e.getMessage());
+        }finally {
+            em.close();
+        }
     }
 }
